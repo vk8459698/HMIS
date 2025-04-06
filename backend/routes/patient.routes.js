0db703690d8d128cd9d0cb29c6faa592fc65c124
Kotha.Hitesh <131675492+Bunny11111@users.noreply.github.com>
2025-04-06 15:12:54 +0530

    Update patient.routes.js

--- backend/routes/patient.routes.js
+++ backend/routes/patient.routes.js
@@ -3,5 +3,11 @@ import express from 'express';
 const router = express.Router();
 
 // TODO: Add routes for patient
+import { FetchPatientProfile, fetchConsultations } from '../controllers/patientController.js';
+
+router.get('/profile/:patientId', FetchPatientProfile);
+router.get('/consultations/:patientId', fetchConsultations);
+
+
 
 export default router;
