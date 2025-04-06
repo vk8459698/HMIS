2c4bfb4857c8936ba89101503135b727e510304e
Jnana-Sai <134124557+Jnana-Sai@users.noreply.github.com>
2025-04-06 23:38:15 +0530

    Update doctor.routes.js

--- backend/routes/doctor.routes.js
+++ backend/routes/doctor.routes.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import { fetchAppointments, updateAppointments } from '../controllers/doctorController.js';
+import { authenticateUser } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 // TODO: Add routes for doctor
+
+router.get('/appointments', authenticateUser, fetchAppointments);
+router.put('/appointments', authenticateUser, updateAppointments);
 
 export default router;
