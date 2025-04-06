d2d82ac367e1aaa5e6f48db4637e16f026151bef
Swarna1806 <143710685+Swarna1806@users.noreply.github.com>
2025-04-06 18:43:53 +0530

    no changes were made

--- backend/routes/pathologist.routes.js
+++ backend/routes/pathologist.routes.js
@@ -1,12 +1,8 @@
 import express from "express";
-import pathologistController from "../controllers/pathologist.controller.js";
+
 
 const router = express.Router();
 
-// Route to check if the patient has an active consultation
-router.get("/consultations/:patientId", pathologistController.getConsultationByPatientId);
 
-// Route to upload a medical test report
-router.post("/reports", pathologistController.uploadReport);
 
 export default router;
